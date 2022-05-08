0abdb2b546d9a38fd11de5671c32c27bd769d81b
Henry Hirsch <henry@w3-net.de>
2022-05-08 19:28:43 +0200

    feat: very basic drum machine

--- drums.js
+++ drums.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
-//  alert("added");
-//    alert("added");
-    var div = document.getElementById('sequence');
+  var div = document.getElementById('sequence');
   var divs = div.getElementsByTagName('div');
     for (var i = 0; i < divs.length; i += 1) {
       divs[i].onclick = function(){
@@ -21,16 +19,13 @@ var currentStep = 0;
 function nextStep(){
   var div = document.getElementById('sequence');
   var divs = div.getElementsByTagName('div');
-  for (var i = 0; i < divs.length; i += 1) {
-    if(i == currentStep) {
-      divs[i].classList.remove("active-step");
-      if(i < 7) {
-        divs[i+1].classList.add("active-step");
-      } else {
-        divs[0].classList.add("active-step");
-      }
-    }
+  divs[currentStep].classList.remove("active-step");
+  if(currentStep < 7) {
+    divs[currentStep+1].classList.add("active-step");
+  } else {
+    divs[0].classList.add("active-step");
   }
+
   currentStep++;
   if(currentStep > 7){
     currentStep = 0;
@@ -38,7 +33,11 @@ function nextStep(){
 }
 
 function playStep(){
-  audio.play();
+  var div = document.getElementById('sequence');
+  var divs = div.getElementsByTagName('div');
+  if(divs[currentStep].classList.contains('enabled-step')){
+    audio.play();
+  };
 }
 
 var timer;
